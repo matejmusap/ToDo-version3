81eb6e7edac0e54ba2b4234e82cf8bd6731c2038
matejmusap <matej.musap@gmail.com>
2018-09-22 23:31:19 +0200

    second phase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from "./TodoList";
+import { Link, Route, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <h1>Welcome to the App!</h1>
+        <p>
+          <Link to="/todos">See my todos!</Link>
         </p>
+        <p>
+          <Link to="/todos/new">Add a todos!</Link>
+        </p>
+        <Route path="/todos" component={TodoList} />
+        <Route exact path="/" render={() => <Redirect to="/todos" />} />
       </div>
     );
   }
